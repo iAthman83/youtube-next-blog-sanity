3f6bb415c0a3156749666032c159eab7e33fed9c
Abu Athman <athman.abu@gmail.com>
2023-06-21 20:58:24 +0300

    create sanity documents

--- sanity-backend-cms/schemas/index.js
+++ sanity-backend-cms/schemas/index.js
@@ -1,1 +1,5 @@
-export const schemaTypes = []
+import blockContent from './blockContent'
+import author from './author'
+import category from './category'
+import post from './post'
+export const schemaTypes = [blockContent, author, category, post]
