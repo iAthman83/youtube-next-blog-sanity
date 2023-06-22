6777356ad5a43ac08d45dbd79b6302a6b43795ac
Mona Lisa <athman.abu@gmail.com>
2023-06-22 18:31:00 +0300

    single page

--- components/BlogCard.jsx
+++ components/BlogCard.jsx
@@ -1,34 +1,37 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const BlogCard = ({ postData }) => {
-  console.log(postData);
   return (
-    <div className="flex flex-col space-y-2 w-72 rounded overflow-hidden">
-      <div>
-        <Image src={postData.image} alt="image" height={50} width={300} />
-      </div>
-      <div className="flex flex-row">
-        <p className="font-semibold text-sm text-primary-brand">
-          {postData.author.name}, {Date(postData.publishedAt).substring(0, 10)}
-        </p>
-      </div>
-      <div>
-        <h1 className="text-xl font-bold">{postData.title}</h1>
-      </div>
-      <div>
-        <p className="line-clamp-2">{postData.body}</p>
-      </div>
-      <div className="flex flex-row flex-wrap space-x-2">
-        {postData.categories.map((category) => (
-          <p
-            key={category._id}
-            className="border bg-primary-dark text-primary-bg py-1 px-2 text-xs rounded-full"
-          >
-            {category.title}
+    <Link href={`/${postData._id}`}>
+      <div className="flex flex-col space-y-2 w-72 rounded overflow-hidden">
+        <div>
+          <Image src={postData.image} alt="image" height={50} width={300} />
+        </div>
+        <div className="flex flex-row">
+          <p className="font-semibold text-sm text-primary-brand">
+            {postData.author.name},{" "}
+            {Date(postData.publishedAt).substring(0, 10)}
           </p>
-        ))}
+        </div>
+        <div>
+          <h1 className="text-xl font-bold">{postData.title}</h1>
+        </div>
+        <div>
+          <p className="line-clamp-2">{postData.body}</p>
+        </div>
+        <div className="flex flex-row flex-wrap space-x-2">
+          {postData.categories.map((category) => (
+            <p
+              key={category._id}
+              className="border bg-primary-dark text-primary-bg py-1 px-2 text-xs rounded-full"
+            >
+              {category.title}
+            </p>
+          ))}
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
